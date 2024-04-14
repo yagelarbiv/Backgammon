5db866d0f581a3e70730b48769b13351c154755f
Elior Tansoura <eliortan177@gmail.com>
2024-04-14 17:42:10 +0300

    Authentication server added to project

---
+++ AuthenticationServer/AuthenticationServer.Services/TokenGenerator/AccessToken/IAccessTokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using AuthenticationServer.Models.Entities;
+using AuthenticationServer.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationServer.Services.TokenGenerator.AccesToken
+{
+    public interface IAccessTokenGenerator
+    {
+        string GenerateAccessToken(AppUser user);
+    }
+}
