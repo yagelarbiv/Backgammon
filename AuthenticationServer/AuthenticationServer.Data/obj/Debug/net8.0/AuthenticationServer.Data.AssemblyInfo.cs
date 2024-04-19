e44497147e09008463bd262694bcc3c435408270
Elior Tansoura <eliortan177@gmail.com>
2024-04-19 16:30:17 +0300

    #

--- AuthenticationServer/AuthenticationServer.Data/obj/Debug/net8.0/AuthenticationServer.Data.AssemblyInfo.cs
+++ AuthenticationServer/AuthenticationServer.Data/obj/Debug/net8.0/AuthenticationServer.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenticationServer.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e9f6cb79b56476a7e9e44ac1bb37f86add70e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c5ac1832f807871367018783f9dadf4d677928")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenticationServer.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenticationServer.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
