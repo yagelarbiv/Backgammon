91020f64c9fc65f30ba006b1933f5077708af276
יגל ארביב <yagelarbiv3@gmail.com>
2024-04-29 16:33:27 +0300

    openning game window

--- ClientSide-Game/src/pages/Game/Game.jsx
+++ ClientSide-Game/src/pages/Game/Game.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import "./Game.css";
 import { io } from "socket.io-client";
 import BoardBottom from "../../components/game-components/BoardBottom";
 import BoardTop from "../../components/game-components/BoardTop";
-import { useParams } from "react-router-dom";
 import LoadingPage from "../../components/game-components/loading/LoadingPage";
 import Timer from "../../components/game-components/timer/Timer";
 import LoadingSpinner from "../../components/game-components/loading/LoadingSpinner";
@@ -14,23 +13,22 @@ import Dice from "../../components/game-components/Dice/Dice";
 import useGameState from "../../hooks/useGameState";
 import { Button } from "@mui/material";
 import toast from "react-hot-toast";
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 function Game() {
-  const { users } = useParams();
-  const socket = io(import.meta.env.VITE_APP_GAME_URL, { withCredentials: true });
-  const [Socket, setSocket] = useState(null);
-  console.log(Socket);
-  const [username, opponent] = useMemo(() => {
-    if (!users) return "";
-    const parsedUsers = users?.split("&");
-    const username = parsedUsers[0];
-    const opponent = parsedUsers[1];
-    return [username, opponent];
-  }, [users]);
+  const query = useQuery();
+  const username = query.get('user');
+  const opponent = query.get('opponent');
+  const token = query.get('token');  // Uncomment if the token is used later
+  console.log(token);
+  const [socket, setSocket] = useState(null);
+
   const { joinGame } = useHttpClient();
-
   const { game, thisTurn, thisMove } = useGameState();
-
   const {
     handleUserJoined,
     handleDiceRoll,
@@ -49,42 +47,51 @@ function Game() {
     player,
   } = useGameEvents(username, opponent);
 
+  // Initialize socket connection only once
   useEffect(() => {
-    setSocket(socket);
-    return () => socket && socket.close();
+    const newSocket = io('http://localhost:3003', {
+            withCredentials: true,
+            transports: ['websocket']  // Ensuring to use WebSockets
+        });
+    setSocket(newSocket);
+    newSocket.emit("connection", () => {
+      console.log("Connected to server");
+    });
+    newSocket.emit("set_name", username);
+    newSocket.on("user-connection", handleUserJoined);
+    newSocket.on("user-rolled-dice", handleDiceRoll);
+    newSocket.on("opponent-select", handleOpponentSelect);
+    newSocket.on("opponent-started-game", opponentStartedGame);
+    newSocket.on("changed-turn", toastMessage);
+    newSocket.on("game-over", toastMessage);
+    newSocket.on("on-game-won", (game) => handleGameOver(game, true));
+    newSocket.on("user-disconnected", handleOpponentLeft);
+
+    return () => {
+      newSocket.off("user-connection", handleUserJoined);
+      newSocket.off("user-rolled-dice", handleDiceRoll);
+      newSocket.off("opponent-select", handleOpponentSelect);
+      newSocket.off("opponent-started-game", opponentStartedGame);
+      newSocket.off("changed-turn", toastMessage);
+      newSocket.off("game-over", toastMessage);
+      newSocket.off("on-game-won");
+      newSocket.off("user-disconnected", handleOpponentLeft);
+      newSocket.disconnect();
+    };
   }, []);
 
   useEffect(() => {
     async function tryJoin() {
-      const joinSuccessful = await joinGame(username, opponent, socket.id);
+      const joinSuccessful = await joinGame(username, opponent, socket?.id);
       if (!joinSuccessful) {
-        toast.error(
-          "Could not join game or game is currently on, try again later."
-        );
+        toast.error("Could not join game or game is currently on, try again later.");
       }
     }
-    tryJoin();
-  }, [socket.id]);
 
-  useEffect(() => {
-    socket.on("user-connection", handleUserJoined);
-    socket.on("user-rolled-dice", handleDiceRoll);
-    socket.on("opponent-select", handleOpponentSelect);
-    socket.on("opponent-started-game", opponentStartedGame);
-    socket.on("changed-turn", toastMessage);
-    socket.on("game-over", toastMessage);
-    socket.on("on-game-won", (game) => handleGameOver(game, true));
-    socket.on("user-disconnected", handleOpponentLeft);
-    return () => {
-      socket.off("user-connection", handleUserStartedGame);
-      socket.off("user-rolled-dice", handleDiceRoll);
-      socket.off("opponent-select", handleOpponentSelect);
-      socket.off("opponent-started-game", opponentStartedGame);
-      socket.off("changed-turn", toastMessage);
-      socket.off("game-over", toastMessage);
-      socket.off("user-disconnected", handleOpponentLeft);
-    };
-  }, []);
+    if (socket) {
+      tryJoin();
+    }
+  }, [socket, username, opponent, joinGame]);
 
   return (
     <>
@@ -97,9 +104,7 @@ function Game() {
               You play as <span className={player}>{player}</span>
             </h1>
             <div className="dice">
-              {thisTurn._dices && canPlay && (
-                <Dice dice={thisTurn._dices}></Dice>
-              )}
+              {thisTurn._dices && canPlay && <Dice dice={thisTurn._dices} />}
             </div>
             <div>
               <Timer timer={timer} />
@@ -111,7 +116,6 @@ function Game() {
             select={handleUserSelect}
             isBlack={player === "Black"}
           />
-
           <BoardBottom
             game={game}
             thisMove={thisMove}
@@ -121,13 +125,7 @@ function Game() {
             canPlay={canPlay}
             isSelecting={isSelecting}
           />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-start",
-              margin: "1em",
-            }}
-          >
+          <div style={{ display: "flex", justifyContent: "flex-start", margin: "1em" }}>
             <Button
               style={{ backgroundColor: "#904E55", color: "#fff" }}
               onClick={() => window.close()}
